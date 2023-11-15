30765446a4536f649afc49a4cd270c72274cc99c
Pedro Kupersmit <pkupersmit@gmail.com>
2023-11-15 17:48:58 -0300

    feat: adding timer

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-var speed: int = 100
+var speed: int = 500
 
 func _ready():
+	print('is this called?')
 	randomize()
 	self.velocity.x  = [-1,1].pick_random()
 	self.velocity.y  = [-0.75,0.75].pick_random()
@@ -12,3 +13,13 @@ func _physics_process(delta):
 	var collision_obj = move_and_collide(self.velocity * speed * delta)
 	if collision_obj:
 		self.velocity = self.velocity.bounce(collision_obj.get_normal())
+	
+
+func restart_ball(): 
+	speed = 500
+	
+	
+func stop_ball():
+	speed = 0
+
+
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,22 +1,20 @@
 extends Node2D
 
-var ball: PackedScene = preload("res://scenes/ball.tscn")
-
-
-
-
-func _ready():
-	ball.instantiate()
-	call_deferred("add_child", ball)
-
 
 func _on_p_1_back_body_entered(body):
 	GameState.set_player_score('p2')
-	body.queue_free()
+	$Ball.position = Vector2(640, 360)
+	get_tree().call_group('BallGroup', 'stop_ball')
+	
 	
 
 
 func _on_p_2_back_body_entered(body):
 	GameState.set_player_score('p1')
-	body.queue_free()
+	$Ball.position = Vector2(640, 360)
+	get_tree().call_group('BallGroup', 'stop_ball')
 	
+
+
+func _on_timer_timeout():
+	get_tree().call_group('BallGroup', 'restart_ball')
