06954299f93116932deae73787571758c561cc20
Pedro <pkupersmit@gmail.com>
2023-11-17 18:23:57 -0300

    change position for velocity to handle movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,17 +1,28 @@
 extends CharacterBody2D
 
-var speed: int = 900
+var speed: float = 600
+var multiplier: int = 50
 
 func _physics_process(delta):
+	
 	if $".".name == 'Player1':
 		player_movement(delta, 'p1_up', 'p1_down')
+		stop_player_movement('p1_up', 'p1_down')
 	else:
 		player_movement(delta, 'p2_up', 'p2_down')
+		stop_player_movement( 'p2_up', 'p2_down')
 	move_and_slide()
-	
 
 func player_movement(delta, input_up, input_down):
 	if Input.is_action_pressed(input_up):
-		position += Vector2(0,-1) * delta * speed
+		print(velocity.y)
+		velocity.y = -1 * speed * delta * multiplier
 	if Input.is_action_pressed(input_down):
-		position += Vector2(0,1) * delta * speed
+		print(velocity.y)
+		velocity.y = 1 * speed * delta * multiplier
+		
+func stop_player_movement(input_up, input_down):
+	if Input.is_action_just_released(input_up):
+		velocity.y = 0
+	if Input.is_action_just_released(input_down):
+		velocity.y = 0
